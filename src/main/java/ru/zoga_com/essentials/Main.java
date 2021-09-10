f83a2badcb1e8e9d2fd3abd796ff8df1bd84cde0
zoga-com <dim.prezident2014@yandex.ru>
2021-09-10 22:52:15 +0300

    /gamemode

--- src/main/java/ru/zoga_com/essentials/Main.java
+++ src/main/java/ru/zoga_com/essentials/Main.java
@@ -2,6 +2,7 @@ package ru.zoga_com.essentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.zoga_com.essentials.commands.CommandFly;
+import ru.zoga_com.essentials.commands.CommandGamemode;
 
 
 public class Main extends JavaPlugin {
@@ -11,6 +12,7 @@ public class Main extends JavaPlugin {
         this.getLogger().info("Enabled.");
         //getServer().getPluginManager().registerEvents(new Handlers(), this);
         this.getCommand("fly").setExecutor(new CommandFly());
+        this.getCommand("gm").setExecutor(new CommandGamemode());
     }
     public void onDisable() {
         this.getLogger().info("Disabled.");
